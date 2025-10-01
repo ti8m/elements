8257b39ee6fb80c013a152b20d21864c15dd3e5a
Marcel Stör <marcelstoer@users.noreply.github.com>
2025-10-01 09:19:23 +0200

    Add support for the optional SchemaFragment#objectRefType property

--- packages/elements-core/src/utils/ref-resolving/ReferenceResolver.ts
+++ packages/elements-core/src/utils/ref-resolving/ReferenceResolver.ts
@@ -1,4 +1,5 @@
-import { resolveInlineRef } from '@stoplight/json';
+import { getLastPathSegment, resolveInlineRef } from '@stoplight/json';
+import { SchemaNodeKind } from '@stoplight/json-schema-tree';
 import { Dictionary } from '@stoplight/types';
 
 export type ReferenceInfo = {
@@ -23,7 +24,18 @@ export const defaultResolver =
 
     const resolved = resolveInlineRef(activeObject as Dictionary<string>, pointer);
 
-    if (resolved) return resolved;
+    if (resolved) {
+      // For resolved objects from a pointer (i.e. referenced by $ref) we add a 'objectRefType' property.
+      // The property value is the resolved type name.
+      // The json-schema-viewer component can then evaluate this property and display more precise information if present.
+      // Example: for '#/components/schemas/Foo' the property value is 'Foo'.
+      // Addresses https://github.com/stoplightio/elements/issues/2762.
+      if (typeof resolved === SchemaNodeKind.Object && pointer) {
+        // @ts-ignore
+        resolved.objectRefType = getLastPathSegment(pointer);
+      }
+      return resolved;
+    }
 
     throw new ReferenceError(`Could not resolve '${pointer}`);
   };
